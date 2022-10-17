9f6a72a0fc910b23590514d6eb150c0005f0ef99
spirit-B <janghoon333@naver.com>
2022-10-17 15:27:15 +0900

    8장 완료

--- src/Info.js
+++ src/Info.js
@@ -1,28 +1,52 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { useReducer } from 'react';
+import useInputs from './useInputs';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect');
-    return () => {
-      console.log('unmount');
-    };
-  }, []);
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = state;
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
+  // const [state, dispatch] = useReducer(reducer, {
+  //   name: '',
+  //   nickname: '',
+  // });
+  // const { name, nickname } = state;
+  // const onChange = (e) => {
+  //   dispatch(e.target);
+  // };
 
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  // const [name, setName] = useState('');
+  // const [nickname, setNickname] = useState('');
+  // useEffect(() => {
+  //   console.log('effect');
+  //   return () => {
+  //     console.log('unmount');
+  //   };
+  // }, []);
+
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  // const onChangeNickname = (e) => {
+  //   setNickname(e.target.value);
+  // };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name='name' value={name} onChange={onChange} />
+        <input name='nickname' value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
